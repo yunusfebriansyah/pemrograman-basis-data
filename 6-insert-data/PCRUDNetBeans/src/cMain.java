d3b7f40f5133870815324d1c946ff654d9af0349
yunusfebriansyah <febriansyahyunus369@gmail.com>
2021-06-08 01:22:44 +0700

    update materi insert data

---
+++ 6-insert-data/PCRUDNetBeans/src/cMain.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import com.view.cView;
+
+public class cMain {
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+        
+        while(true){
+
+            System.out.print("\n=== MENU ===\n"
+            + "1. Lihat Semua Data Barang\n"
+            + "2. Detail Data Barang\n"
+            + "3. Cari Data Barang\n"
+            + "4. Tambah Data Barang\n"
+            + "5. Update Data Barang\n"
+            + "0. Exit\n"
+            + "Pilih[1/2/3/4/5/0] : ");
+
+            String pilihan = input.next();
+
+            if( pilihan.equalsIgnoreCase("0") ){
+                System.out.println("Terimakasih!!");
+                break;
+            }
+
+            switch (pilihan) {
+                case "1" :
+                    cView.getAllData();
+                    break;
+                case "2" :
+                    cView.detailData();
+                    break;
+                case "3" :
+                    cView.cariData();
+                    break;
+                case "4" :
+                    cView.tambahData();
+                    break;
+                case "5" :
+                    System.out.println("Update Data Barang");
+                    break;
+                default:
+                    System.out.println("Pilihan salah!!");
+                    break;
+            }
+
+            
+        }
+        
+        // close input scanner nya
+        input.close();
+    }
+}
---
+++ 6-insert-data/PCRUDNetBeans/src/com/view/cView.java
@@ -0,0 +1,63 @@
+package com.view;
+
+import java.util.Scanner;
+
+import com.config.cConfig;
+
+public class cView {
+
+  public static Scanner input = new Scanner(System.in);
+  
+  public static void getAllData()
+  {
+    //  pesan header
+    System.out.println("\n::: DATA BARANG :::");
+    // data semua barangnya
+    System.out.println(cConfig.getAllData()); 
+
+  }
+
+
+  public static void detailData()
+  {
+    System.out.println("\n::: DETAIL DATA BARANG:::");
+
+    System.out.print("Masukkan ID Barang : ");
+    int id = input.nextInt();
+    
+    System.out.println("Hasil Data");
+    System.out.println(cConfig.detailData(id));
+  }
+
+  public static void cariData()
+  {
+    System.out.println("\n::: CARI DATA :::");
+    System.out.print("Masukkan Keyword : ");
+    String keyword = input.nextLine();
+
+    System.out.println("\nHasil Data");
+    System.out.println(cConfig.cariData(keyword));
+  }
+  
+  public static void tambahData()
+  {
+
+    System.out.println("\n::: TAMBAH DATA BARANG :::");
+    System.out.print("Masukkan Nama Barang : ");
+    String namaBarang = input.nextLine();
+    System.out.print("Masukkan Stok Barang : ");
+    int stokBarang = input.nextInt();
+    System.out.print("Masukkan Harga Barang : ");
+    int hargaBarang = input.nextInt();
+
+    if ( cConfig.tambahData(namaBarang, stokBarang, hargaBarang) ){
+      System.out.println("Data Barang berhasil ditambahkan!!");
+      cView.getAllData();
+    }else{
+      System.out.println("Data Barang gagal ditambahkan!!");
+    }
+
+  }
+
+
+}
