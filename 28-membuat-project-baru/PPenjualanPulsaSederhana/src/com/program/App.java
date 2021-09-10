13f008d91a560e6fb10ed4055e5f06657d02d779
yunusfebriansyah <febriansyahyunus369@gmail.com>
2021-09-11 01:30:28 +0700

    update 28. membuat project baru beserta konfigurasinya

---
+++ 28-membuat-project-baru/PPenjualanPulsaSederhana/src/com/program/App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.program;
+
+/**
+ *
+ * @author Yunus Febriansyah
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!!");
+        
+    }
+    
+}
