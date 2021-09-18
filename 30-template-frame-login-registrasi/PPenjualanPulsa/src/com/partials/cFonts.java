b0e4cb1ac1382999b586bb248b88b1b2fd5f2b46
yunusfebriansyah <febriansyahyunus369@gmail.com>
2021-09-19 00:36:40 +0700

    update 30. Membuat Template Frame Login dan Registrasi

---
+++ 30-template-frame-login-registrasi/PPenjualanPulsa/src/com/partials/cFonts.java
@@ -0,0 +1,8 @@
+package com.partials;
+import java.awt.Font;
+
+public class cFonts {
+  
+  public static final Font TITLE_START_FONT = new Font("Poppins Semibold", Font.PLAIN, 26);
+
+}
---
+++ 30-template-frame-login-registrasi/PPenjualanPulsa/src/com/program/App.java
@@ -0,0 +1,13 @@
+package com.program;
+import com.templates.cStartFrame;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    cStartFrame templateStart = new cStartFrame("Template Start");
+    templateStart.setVisible(true);
+
+  }
+
+}
